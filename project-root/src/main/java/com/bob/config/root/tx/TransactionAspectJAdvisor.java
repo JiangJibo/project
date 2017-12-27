9f440aa07676911361e94cc3bb32ee56d1ce807b
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2017-12-27 16:36:55 +0800

    ''

--- project-root/src/main/java/com/bob/config/root/tx/TransactionAspectJAdvisor.java
+++ project-root/src/main/java/com/bob/config/root/tx/TransactionAspectJAdvisor.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
  */
 @Aspect
 @Component
-public class TransactionAspectJAdvice {
+public class TransactionAspectJAdvisor {
 
     @Autowired
     private TransactionAspectInvoker transactionAspectInvoker;
--- project-root/src/main/java/com/bob/config/root/tx/TransactionConfiguration.java
+++ project-root/src/main/java/com/bob/config/root/tx/TransactionConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 import org.springframework.transaction.interceptor.MatchAlwaysTransactionAttributeSource;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 /**
  * 事务通知
@@ -17,53 +16,13 @@ import org.springframework.transaction.interceptor.TransactionInterceptor;
 @Configuration
 public class TransactionConfiguration {
 
-    private static final String[] TRANSACTION_INCLUDE_PATTERNS = {"com.bob.mvc.service.impl..*.*(..)"};
-    private static final String[] TRANSACTION_EXCLUDE_PATTERNS = {};
-
-    /**
-     * 设置事务切面
-     *
-     * @param defaultTransactionInterceptor
-     * @return
-     */
-    //@Bean
-   /* public TransactionJdkRegexpAdvisor transactionJdkRegexpAdvisor(TransactionInterceptor defaultTransactionInterceptor) {
-        TransactionJdkRegexpAdvisor txAdvisor = new TransactionJdkRegexpAdvisor();
-        txAdvisor.setIncludePatters(TRANSACTION_INCLUDE_PATTERNS);
-        //txAdvisor.setExcludePattern(TRANSACTION_EXCLUDE_PATTERNS);
-        txAdvisor.setAdvice(defaultTransactionInterceptor);
-        return txAdvisor;
-    }*/
-
-    /**
-     * 事务通知
-     *
-     * @return
-     */
-    //@Bean
-    public TransactionInterceptor defaultTransactionInterceptor() {
-        //设置事务的属性
-        DefaultTransactionAttribute transactionAttribute = new DefaultTransactionAttribute();
-        transactionAttribute.setIsolationLevel(Connection.TRANSACTION_REPEATABLE_READ);
-        transactionAttribute.setTimeout(3000);
-        //设置事务源
-        MatchAlwaysTransactionAttributeSource txAttrBs = new MatchAlwaysTransactionAttributeSource();
-        txAttrBs.setTransactionAttribute(transactionAttribute);
-        //设置通知
-        TransactionInterceptor interceptor = new TransactionInterceptor();
-        interceptor.setTransactionAttributeSource(txAttrBs);
-        return interceptor;
-    }
-
-    //############################################ 另一种尝试 ######################################################
-
     @Bean
     public TransactionAspectInvoker transactionAspectInvoker() {
         TransactionAspectInvoker txInvoker = new TransactionAspectInvoker();
         //设置事务的属性
         DefaultTransactionAttribute transactionAttribute = new DefaultTransactionAttribute();
         transactionAttribute.setIsolationLevel(Connection.TRANSACTION_REPEATABLE_READ);
-        transactionAttribute.setTimeout(30000);
+        transactionAttribute.setTimeout(3000);
         //设置事务源
         MatchAlwaysTransactionAttributeSource txAttrBs = new MatchAlwaysTransactionAttributeSource();
         txAttrBs.setTransactionAttribute(transactionAttribute);
