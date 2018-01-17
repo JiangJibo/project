5765ba872759bbb0f6c50ded1f3b00c5de210059
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-01-17 10:21:58 +0800

    ''

--- project-root/src/main/java/com/bob/config/root/tx/TransactionAspectInvoker.java
+++ project-root/src/main/java/com/bob/config/root/tx/TransactionAspectInvoker.java
@@ -3,6 +3,9 @@ package com.bob.config.root.tx;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.aop.support.AopUtils;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.MatchAlwaysTransactionAttributeSource;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 /**
@@ -12,6 +15,22 @@ import org.springframework.transaction.interceptor.TransactionAspectSupport;
  * @create 2017-12-27 11:17
  */
 public class TransactionAspectInvoker extends TransactionAspectSupport {
+
+    public TransactionAspectInvoker() {
+        //设置事务的属性
+        DefaultTransactionAttribute transactionAttribute = new DefaultTransactionAttribute();
+        //不设置事务的隔离级别,默认使用数据库的设置
+        //transactionAttribute.setIsolationLevel(Connection.TRANSACTION_REPEATABLE_READ);
+        //设置事务的超时时间,单位秒,注意是事务不是连接的超时设置
+        transactionAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        transactionAttribute.setTimeout(300);
+        //设置事务源
+        MatchAlwaysTransactionAttributeSource txAttrBs = new MatchAlwaysTransactionAttributeSource();
+        txAttrBs.setTransactionAttribute(transactionAttribute);
+        setTransactionAttributeSource(txAttrBs);
+        //可以配置多个事务属性源,对不同的方法生成不同的事务属性
+        //txInvoker.setTransactionAttributeSources(new TransactionAttributeSource[] {});
+    }
 
     /**
      * @param joinPoint
