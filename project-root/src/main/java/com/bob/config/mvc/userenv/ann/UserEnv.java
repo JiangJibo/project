e574c39112f335fc9dccd39be2e634aa294de4d8
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-01-29 10:27:14 +0800

    ''

--- project-root/src/main/java/com/bob/config/mvc/userenv/ann/UserEnv.java
+++ project-root/src/main/java/com/bob/config/mvc/userenv/ann/UserEnv.java
@@ -1,7 +1,3 @@
-/**
- * Copyright(C) 2016 Fugle Technology Co. Ltd. All rights reserved.
- *
- */
 package com.bob.config.mvc.userenv.ann;
 
 import java.lang.annotation.ElementType;
@@ -10,20 +6,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @since 2016年12月7日 下午2:01:13
- * @version $Id$
  * @author JiangJibo
- *
+ * @version $Id$
+ * @since 2016年12月7日 下午2:01:13
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER })
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 public @interface UserEnv {
 
-	/**
-	 * 环境变量的名称
-	 * 
-	 * @return
-	 */
-	String value() default "";
+    /**
+     * 环境变量的名称
+     *
+     * @return
+     */
+    String value() default "";
 
 }
