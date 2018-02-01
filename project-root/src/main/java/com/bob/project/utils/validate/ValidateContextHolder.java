533c76adac7fc3d4de97498c2516e42f5b72997a
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-02-01 11:05:52 +0800

    ''

--- project-root/src/main/java/com/bob/project/utils/validate/ValidateContextHolder.java
+++ project-root/src/main/java/com/bob/project/utils/validate/ValidateContextHolder.java
@@ -11,7 +11,7 @@ import java.util.Set;
  * @author wb-jjb318191
  * @create 2018-01-31 13:43
  */
-public class ValidateContextHolder {
+class ValidateContextHolder {
 
     private static Map<Class<?>, Set<ValidatedElement>> VALIDATED_CLASS_MAPPINGS = new HashMap<>();
     private static Map<Method, Set<ValidatedElement>> VALIDATED_METHOD_MAPPINGS = new HashMap<>();
@@ -20,7 +20,7 @@ public class ValidateContextHolder {
      * @param clazz
      * @param elements
      */
-    public static void addClassMapping(Class<?> clazz, Set<ValidatedElement> elements) {
+    static void addClassMapping(Class<?> clazz, Set<ValidatedElement> elements) {
         VALIDATED_CLASS_MAPPINGS.put(clazz, elements);
     }
 
@@ -28,7 +28,7 @@ public class ValidateContextHolder {
      * @param clazz
      * @return
      */
-    public static Set<ValidatedElement> getClassMapping(Class<?> clazz) {
+    static Set<ValidatedElement> getClassMapping(Class<?> clazz) {
         return VALIDATED_CLASS_MAPPINGS.get(clazz);
     }
 
@@ -36,7 +36,7 @@ public class ValidateContextHolder {
      * @param method
      * @param elements
      */
-    public static void addMethodMapping(Method method, Set<ValidatedElement> elements) {
+    static void addMethodMapping(Method method, Set<ValidatedElement> elements) {
         VALIDATED_METHOD_MAPPINGS.put(method, elements);
     }
 
@@ -44,7 +44,7 @@ public class ValidateContextHolder {
      * @param method
      * @return
      */
-    public static Set<ValidatedElement> getMethodMapping(Method method) {
+    static Set<ValidatedElement> getMethodMapping(Method method) {
         return VALIDATED_METHOD_MAPPINGS.get(method);
     }
 
--- project-root/src/main/java/com/bob/project/utils/validate/ValidatedElement.java
+++ project-root/src/main/java/com/bob/project/utils/validate/ValidatedElement.java
@@ -11,28 +11,28 @@ import java.util.List;
  * @author wb-jjb318191
  * @create 2018-01-31 13:36
  */
-public class ValidatedElement {
+class ValidatedElement {
 
     private Field field;
     private List<Annotation> annotations;
 
-    public ValidatedElement(Field field) {
+    ValidatedElement(Field field) {
         this.field = field;
     }
 
-    public ValidatedElement(Field field, List<Annotation> annotations) {
+    ValidatedElement(Field field, List<Annotation> annotations) {
         this.field = field;
         this.annotations = annotations;
     }
 
-    public void addAnnotation(Annotation ann) {
+    void addAnnotation(Annotation ann) {
         if (annotations == null) {
             annotations = new ArrayList<>();
         }
         annotations.add(ann);
     }
 
-    public boolean isQulified() {
+    boolean isQulified() {
         return annotations != null;
     }
 
@@ -44,7 +44,7 @@ public class ValidatedElement {
         this.field = field;
     }
 
-    public List<Annotation> getAnnotations() {
+    List<Annotation> getAnnotations() {
         return annotations;
     }
 
