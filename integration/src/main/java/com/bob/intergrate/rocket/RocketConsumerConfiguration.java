d55e960ff03af979d90fcb9c0a633cf90c0e6067
JiangJibo <q450210669@gmail.com>
2018-03-20 22:43:11 +0800

    ''

--- integration/src/main/java/com/bob/intergrate/rocket/RocketConsumerConfiguration.java
+++ integration/src/main/java/com/bob/intergrate/rocket/RocketConsumerConfiguration.java
@@ -1,6 +1,7 @@
 package com.bob.intergrate.rocket;
 
 import com.bob.intergrate.rocket.ann.RocketListener;
+import com.bob.intergrate.rocket.constant.RocketDefinitionConstant;
 import com.google.gson.Gson;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
@@ -17,9 +18,18 @@ public class RocketConsumerConfiguration {
 
     private Gson gson = new Gson();
 
+    /**
+     * 通过{@link RocketListener}形式定义的Consumer,名称形式以为：
+     * consumeMethodName + {@link RocketDefinitionConstant#ROCKETMQ_CONSUMER_BEAN_NAME_SUFFIX}
+     */
     @Autowired
     private DefaultMQPushConsumer serviceRocketConsumer;
 
+    /**
+     * @param msg
+     * @param context
+     * @return true:消费成功 ;  false:消费失败,发回给Broker,一段时间后重试
+     */
     @RocketListener(consumerGroup = "rmq-group", topic = "test-topic")
     public boolean service(MessageExt msg, ConsumeConcurrentlyContext context) {
         System.out.println(gson.toJson(msg));
