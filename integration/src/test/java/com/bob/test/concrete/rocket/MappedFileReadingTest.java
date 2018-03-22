6b4db3238e15645f62c20545dcae581577f9ae29
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-03-22 15:21:29 +0800

    ''

--- integration/src/test/java/com/bob/test/concrete/rocket/MappedFileReadingTest.java
+++ integration/src/test/java/com/bob/test/concrete/rocket/MappedFileReadingTest.java
@@ -3,15 +3,28 @@ package com.bob.test.concrete.rocket;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import java.util.Date;
+import java.util.Map;
 
 import com.google.gson.Gson;
+import org.apache.rocketmq.common.UtilAll;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.sysflag.MessageSysFlag;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.apache.rocketmq.common.message.MessageDecoder.MSG_ID_LENGTH;
+import static org.apache.rocketmq.common.message.MessageDecoder.createMessageId;
+import static org.apache.rocketmq.common.message.MessageDecoder.string2messageProperties;
 
 /**
  * MappedFile信息读取,包括CommitLog,ConsumeQueue,IndexFile
@@ -123,6 +136,98 @@ public class MappedFileReadingTest {
         byte[] properties = new byte[propertiesLength];
         mappedByteBuffer.get(properties);
         LOGGER.debug("Message Properties:[{}]", new String(properties));
+    }
+
+    public void readCommitLog0() throws UnknownHostException, UnsupportedEncodingException {
+        MessageExt msgExt = new MessageExt();
+        // 1 TOTALSIZE
+        int storeSize = mappedByteBuffer.getInt();
+        msgExt.setStoreSize(storeSize);
+
+        // 2 MAGICCODE
+        mappedByteBuffer.getInt();
+
+        // 3 BODYCRC
+        int bodyCRC = mappedByteBuffer.getInt();
+        msgExt.setBodyCRC(bodyCRC);
+
+        // 4 QUEUEID
+        int queueId = mappedByteBuffer.getInt();
+        msgExt.setQueueId(queueId);
+
+        // 5 FLAG
+        int flag = mappedByteBuffer.getInt();
+        msgExt.setFlag(flag);
+
+        // 6 QUEUEOFFSET
+        long queueOffset = mappedByteBuffer.getLong();
+        msgExt.setQueueOffset(queueOffset);
+
+        // 7 PHYSICALOFFSET
+        long physicOffset = mappedByteBuffer.getLong();
+        msgExt.setCommitLogOffset(physicOffset);
+
+        // 8 SYSFLAG
+        int sysFlag = mappedByteBuffer.getInt();
+        msgExt.setSysFlag(sysFlag);
+
+        // 9 BORNTIMESTAMP
+        long bornTimeStamp = mappedByteBuffer.getLong();
+        msgExt.setBornTimestamp(bornTimeStamp);
+
+        // 10 BORNHOST
+        byte[] bornHost = new byte[4];
+        mappedByteBuffer.get(bornHost, 0, 4);
+        int port = mappedByteBuffer.getInt();
+        msgExt.setBornHost(new InetSocketAddress(InetAddress.getByAddress(bornHost), port));
+
+        // 11 STORETIMESTAMP
+        long storeTimestamp = mappedByteBuffer.getLong();
+        msgExt.setStoreTimestamp(storeTimestamp);
+
+        // 12 STOREHOST
+        byte[] storeHost = new byte[4];
+        mappedByteBuffer.get(storeHost, 0, 4);
+        port = mappedByteBuffer.getInt();
+        msgExt.setStoreHost(new InetSocketAddress(InetAddress.getByAddress(storeHost), port));
+
+        // 13 RECONSUMETIMES
+        int reconsumeTimes = mappedByteBuffer.getInt();
+        msgExt.setReconsumeTimes(reconsumeTimes);
+
+        // 14 Prepared Transaction Offset
+        long preparedTransactionOffset = mappedByteBuffer.getLong();
+        msgExt.setPreparedTransactionOffset(preparedTransactionOffset);
+
+        // 15 BODY
+        int bodyLen = mappedByteBuffer.getInt();
+        if (bodyLen > 0) {
+            byte[] body = new byte[bodyLen];
+            mappedByteBuffer.get(body);
+            msgExt.setBody(body);
+        }
+
+        // 16 TOPIC
+        byte topicLen = mappedByteBuffer.get();
+        byte[] topic = new byte[(int)topicLen];
+        mappedByteBuffer.get(topic);
+        msgExt.setTopic(new String(topic, "UTF-8"));
+
+        // 17 properties
+        short propertiesLength = mappedByteBuffer.getShort();
+        if (propertiesLength > 0) {
+            byte[] properties = new byte[propertiesLength];
+            mappedByteBuffer.get(properties);
+            String propertiesString = new String(properties, "UTF-8");
+            Map<String, String> map = string2messageProperties(propertiesString);
+            System.out.println(map);
+            //msgExt.setProperties(map);
+        }
+
+        ByteBuffer byteBufferMsgId = ByteBuffer.allocate(MSG_ID_LENGTH);
+        String msgId = createMessageId(byteBufferMsgId, msgExt.getStoreHostBytes(), msgExt.getCommitLogOffset());
+        msgExt.setMsgId(msgId);
+
     }
 
 }
