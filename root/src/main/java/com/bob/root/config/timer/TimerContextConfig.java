def186c0cfc2bed6585c639562bd269ff09ce7c0
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-03-28 15:30:40 +0800

    'Maven依赖去重'

--- root/src/main/java/com/bob/root/config/timer/TimerContextConfig.java
+++ root/src/main/java/com/bob/root/config/timer/TimerContextConfig.java
@@ -1,16 +1,10 @@
 package com.bob.root.config.timer;
 
-import com.bob.root.config.timer.quartz.QuartzTaskExample;
-import org.quartz.CronTrigger;
-import org.quartz.JobDetail;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
-import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 /**
  * 定时器配置,推荐使用@Schedule的形式配置
@@ -41,36 +35,5 @@ public class TimerContextConfig {
 		return taskSchedule;
 	}
 
-	/**
-	 * 
-	 * Quartz定时器配置
-	 * 
-	 */
-
-	// @Bean
-	public MethodInvokingJobDetailFactoryBean taskInfo(QuartzTaskExample quartzTaskExample) {
-		MethodInvokingJobDetailFactoryBean taskInfo = new MethodInvokingJobDetailFactoryBean();
-		taskInfo.setTargetObject(quartzTaskExample);
-		taskInfo.setTargetMethod("execute");
-		return taskInfo;
-
-	}
-
-	// @Bean
-	public CronTriggerFactoryBean quartzTrigger(JobDetail taskInfo) {
-		CronTriggerFactoryBean quartzTrigger = new CronTriggerFactoryBean();
-		quartzTrigger.setJobDetail(taskInfo);
-		quartzTrigger.setCronExpression("0/20 * * * * ?");
-		return quartzTrigger;
-
-	}
-
-	// @Bean
-	public SchedulerFactoryBean registerQuartz(CronTrigger quartzTrigger) {
-		SchedulerFactoryBean registerQuartz = new SchedulerFactoryBean();
-		registerQuartz.setTriggers(quartzTrigger);
-		return registerQuartz;
-
-	}
 
 }
