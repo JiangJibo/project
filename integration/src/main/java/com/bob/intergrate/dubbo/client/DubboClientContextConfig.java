82ef36a63c16e645ef4f073a1fdd221594533e32
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-05-03 18:03:28 +0800

    ''

--- integration/src/main/java/com/bob/intergrate/dubbo/client/DubboClientContextConfig.java
+++ integration/src/main/java/com/bob/intergrate/dubbo/client/DubboClientContextConfig.java
@@ -2,10 +2,13 @@ package com.bob.intergrate.dubbo.client;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * @author wb-jjb318191
  * @create 2018-05-03 16:54
  */
 @EnableDubbo
+@PropertySource("classpath:dubbo-client.properties")
 public class DubboClientContextConfig {
 }
--- integration/src/main/java/com/bob/intergrate/dubbo/server/DubboServerContextConfig.java
+++ integration/src/main/java/com/bob/intergrate/dubbo/server/DubboServerContextConfig.java
@@ -2,10 +2,13 @@ package com.bob.intergrate.dubbo.server;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * @author wb-jjb318191
  * @create 2018-05-03 16:52
  */
 @EnableDubbo
+@PropertySource("classpath:dubbo-server.properties")
 public class DubboServerContextConfig {
 }
