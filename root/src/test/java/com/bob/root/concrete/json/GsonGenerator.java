caf9b807b3a568912b3a125783fcf25341f714e7
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-05-29 16:43:50 +0800

    ''

---
+++ root/src/test/java/com/bob/root/concrete/json/GsonGenerator.java
@@ -0,0 +1,51 @@
+package com.bob.root.concrete.json;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+/**
+ * 生成Gson对象，包含对时间戳与时间的转换器
+ *
+ * @author wb-jjb318191
+ * @create 2018-05-29 16:39
+ */
+public class GsonGenerator {
+
+    /**
+     * @return
+     */
+    public static Gson newGsonInstance() {
+        GsonBuilder gb = new GsonBuilder();
+        gb.registerTypeAdapter(Date.class, new DateSerializer()).setDateFormat(DateFormat.LONG);
+        gb.registerTypeAdapter(Date.class, new DateDeserializer()).setDateFormat(DateFormat.LONG);
+        return gb.create();
+    }
+
+    static class DateDeserializer implements JsonDeserializer<Date> {
+
+        @Override
+        public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return new Date(json.getAsJsonPrimitive().getAsLong());
+        }
+    }
+
+    static class DateSerializer implements JsonSerializer<Date> {
+
+        @Override
+        public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.getTime());
+        }
+    }
+
+}
