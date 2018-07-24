460d7bfac84f36e55ebf84f7fcab69e32b6e6106
JiangJibo <q450210669@gmail.com>
2018-07-24 23:08:03 +0800

    调整线程池CoreSize

---
+++ root/src/test/java/com/bob/root/concrete/nio/DirectBufferTest.java
@@ -0,0 +1,30 @@
+package com.bob.root.concrete.nio;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+/**
+ * 直接内存测试
+ *
+ * @author Administrator
+ * @create 2018-07-24 20:51
+ */
+public class DirectBufferTest {
+
+    @Test
+    public void testDirectBuffer() {
+        loopAllocateDirect();
+    }
+
+    private void allocateDirect() {
+        ByteBuffer.allocateDirect(1024 * 1024 * 200); // 100MB
+    }
+
+    private void loopAllocateDirect() {
+        for (int i = 0; i < 20000; i++) {
+            ByteBuffer.allocateDirect(1024 * 100);  //100K
+        }
+    }
+
+}
